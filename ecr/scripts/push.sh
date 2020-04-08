ce855ad8b2ecf85b33cf5118fe939b0f9d02eff8
Mark Sost <sostmark@gmail.com>
2020-04-08 08:12:17 -0400

    PLAT-804 - Initial commit

---
+++ ecr/scripts/push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+docker push $DOCKER_IMAGE_BASE:$DOCKER_TAG
+
+if [[ ! -z "${CIRCLE_TAG}" ]]; then
+  docker tag $DOCKER_IMAGE_BASE:$DOCKER_TAG $DOCKER_IMAGE_BASE:stable
+  docker push $DOCKER_IMAGE_BASE:stable
+elif [ "${CIRCLE_BRANCH}" == "master" ]; then
+  docker tag $DOCKER_IMAGE_BASE:$DOCKER_TAG $DOCKER_IMAGE_BASE:latest
+  docker push $DOCKER_IMAGE_BASE:latest
+fi
