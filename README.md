b884d480adfa2285ef6a707ecb3c43374f0269e2
Mark Sost <sostmark@gmail.com>
2020-04-08 09:50:12 -0400

    Updates tagging structure, circle example, and README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# marksost
-ci-tools
+# ci-tools
+
+Continuous Integration tooling for LeafLink source code repositories
+
+---
+
+[![Current Release](https://img.shields.io/badge/release-0.1.0-1eb0fc.svg)](https://github.com/leaflink/ci-tools/releases/tag/0.1.0)
+[![CircleCI](https://circleci.com/gh/LeafLink/ci-tools.svg?style=svg)](https://circleci.com/gh/LeafLink/ci-tools)
--- ecr/scripts/setup.sh
+++ ecr/scripts/setup.sh
@@ -6,7 +6,7 @@ AWS_ECR_BASE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
 CIRCLE_TAG="$(git tag -l --points-at HEAD)" # NOTE: CIRCLE_TAG is flaky, so we override it here
 DOCKER_TAG="${CIRCLE_SHA1:0:7}"
 
-[ "${CIRCLE_BRANCH}" == "master" ] && DOCKER_TAG="master"
+[ "${CIRCLE_BRANCH}" == "master" ] && DOCKER_TAG="master-${CIRCLE_SHA1:0:7}"
 [[ ! -z "${CIRCLE_PR_NUMBER}" ]] && DOCKER_TAG="PR-${CIRCLE_PR_NUMBER}"
 [[ ! -z "${CIRCLE_TAG}" ]] && DOCKER_TAG="${CIRCLE_TAG}"
 
