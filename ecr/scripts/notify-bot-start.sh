7b84a017836630be13f0d9fb90d93bb4d7dc3980
Mark Sost <sostmark@gmail.com>
2020-06-16 14:52:42 -0400

    Test 3

--- ecr/scripts/notify-bot-start.sh
+++ ecr/scripts/notify-bot-start.sh
@@ -8,4 +8,9 @@ if [[ ! -z "${BOT_WEBHOOK_URL}" ]] && [[ ! -z "${BOT_WEBHOOK_TOKEN}" ]]; then
     echo "    DEBUG: Webhook URL - ${BOT_WEBHOOK_URL}"
     echo "    DEBUG: Tag - ${DOCKER_TAG}"
     echo "    DEBUG: Repo - ${CIRCLE_PROJECT_REPONAME}"
+
+    curl -X POST \
+        -h "Authorization: Bearer ${BOT_WEBHOOK_TOKEN}" -h "Content-Type: application/json" \
+        -d "{\"event\": \"build_started\", \"repo\": \"${CIRCLE_PROJECT_REPONAME}\", \"tag\": \"${DOCKER_TAG}\"}" \
+        $BOT_WEBHOOK_URL
 fi
